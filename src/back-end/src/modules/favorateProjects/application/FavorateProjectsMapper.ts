b2ea55c096bc7e2b41bc2118c8d2d7a41a217abd
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-08-18 10:26:25 -0300

    feat(backend) [ favorateProjects ]: add new mapper.

--- src/back-end/src/modules/favorateProjects/application/FavorateProjectsMapper.ts
+++ src/back-end/src/modules/favorateProjects/application/FavorateProjectsMapper.ts
@@ -1,6 +1,7 @@
 import { CreateFavorateProjectDTO } from "@favorateProjects/api/FavorateProjectsDTO";
 import { FavorateProjects } from "@favorateProjects/domain/entities/FavorateProjects";
 import { FavorateProjects as FavorateProjectsModel } from "@prisma/client";
+import { FavorateProjectsContract } from "@shared/contracts/FavorateProjectsContract";
 
 export const FavorateProjectsMapper = {
   fromDomainToPrisma(domain: FavorateProjects): FavorateProjectsModel {
@@ -19,5 +20,11 @@ export const FavorateProjectsMapper = {
     dto: CreateFavorateProjectDTO
   ): FavorateProjects {
     return new FavorateProjects("", dto.userId, dto.projectId);
+  },
+
+  fromPrismaToContract(model: FavorateProjectsModel): FavorateProjectsContract {
+    return {
+      ...model,
+    };
   },
 };
