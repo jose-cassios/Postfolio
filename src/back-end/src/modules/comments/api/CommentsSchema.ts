b3b170c2e0c1dc70a93be442c4277a0276799a37
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-08-19 07:52:20 -0300

    feat(backend) [ comments ]: add schemas.

---
+++ src/back-end/src/modules/comments/api/CommentsSchema.ts
@@ -0,0 +1,59 @@
+import { FastifyRequest } from "fastify";
+import z from "zod";
+
+const CreateCommentsBodySchema = z.object({
+  content: z
+    .string()
+    .min(1, "Comentario muito curto!")
+    .max(500, "Comentario muito longo!"),
+
+  project: z.string(),
+});
+
+type CreateCommentsRequest = FastifyRequest<{
+  Body: z.infer<typeof CreateCommentsBodySchema>;
+}>;
+
+const UpdateCommentsParamsSchema = z.object({
+  commentId: z.string(),
+});
+
+const UpdateCommentsBodySchema = z.object({
+  content: z
+    .string()
+    .min(1, "Comentario muito curto!")
+    .max(500, "Comentario muito longo!"),
+});
+
+type UpdateCommentRequest = FastifyRequest<{
+  Params: z.infer<typeof UpdateCommentsParamsSchema>;
+  Body: z.infer<typeof UpdateCommentsBodySchema>;
+}>;
+
+const DeleteCommentsParamsSchema = z.object({
+  commentId: z.string(),
+});
+
+type DeleteCommentsRequest = FastifyRequest<{
+  Params: z.infer<typeof DeleteCommentsParamsSchema>;
+}>;
+
+const commentsRouteSchema = {
+  create: {
+    body: CreateCommentsBodySchema,
+  },
+  update: {
+    params: UpdateCommentsParamsSchema,
+    body: UpdateCommentsBodySchema,
+  },
+  delete: {
+    DeleteCommentsParamsSchema,
+  },
+};
+
+export {
+  commentsRouteSchema,
+  CreateCommentsRequest,
+  UpdateCommentRequest,
+  DeleteCommentsParamsSchema,
+};
