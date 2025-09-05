8dcfd2b4796057fc95ea6c53fab4430b06aca3d6
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-09-05 17:25:02 -0300

    feat(back-end): create index.

---
+++ src/back-end/src/index.ts
@@ -0,0 +1,11 @@
+import { app } from "./app";
+
+export default async (req: any, res: any) => {
+  try {
+    await app.ready();
+    app.server.emit("request", req, res);
+  } catch (error) {
+    console.error("Não foi possivel emitir a requisição. err: ", error);
+    res.status(500).send("Não foi possivel emitir a requisição.");
+  }
+};
