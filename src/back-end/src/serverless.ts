7ac0ae9286e056dd96e8c16470e9b3b8e1b78f7c
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-09-06 12:42:20 -0300

    fix(back-end): build adjustment v18.

--- src/back-end/src/serverless.ts
+++ src/back-end/src/serverless.ts
@@ -1,12 +1,13 @@
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import { app } from "app";
 
-const app = Fastify({
-  logger: true,
-});
+// const app = Fastify({
+//   logger: true,
+// });
 
-app.get("/", async (req, reply) => {
-  return reply.status(200).send({ msg: "Deu certo" });
-});
+// app.get("/", async (req, reply) => {
+//   return reply.status(200).send({ msg: "Deu certo" });
+// });
 
 export default async function handler(req: any, res: any) {
   await app.ready();
