0b3b9000f69d96abed99943756560841f4ad326c
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-09-06 16:23:23 -0300

    fix(back-end): build adjustment v25.

---
+++ src/back-end/src/api/index.ts
@@ -0,0 +1,35 @@
+import { app } from "../app";
+
+// export default async function handler(req: any, res: any) => {
+//   try {
+//     await app.ready();
+//     app.server.emit("request", req, res);
+//   } catch (error) {
+//     console.error("Não foi possivel emitir a requisição. err: ", error);
+//     res.status(500).send("Não foi possivel emitir a requisição.");
+//   }
+// };
+
+// const app = Fastify({
+//   logger: true,
+// });
+
+// app.get("/", async (req, reply) => {
+//   return reply.status(200).send({ msg: "Deu certo" });
+// });
+
+export default async function handler(req: any, res: any) {
+  await app.ready();
+  app.server.emit("request", req, res);
+}
+// export default app;
+
+// export default async (req: FastifyRequest, res: FastifyReply) => {
+//   try {
+//     await app.ready();
+//     app.server.emit("request", req, res);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ error: "Internal Server Error" });
+//   }
+// };
