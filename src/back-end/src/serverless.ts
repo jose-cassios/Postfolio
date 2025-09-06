20af834866b81ea4ba161419145fdd3e7c23e2fc
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-09-06 12:13:14 -0300

    fix(back-end): build adjustment v15.

--- src/back-end/src/serverless.ts
+++ src/back-end/src/serverless.ts
@@ -8,12 +8,14 @@ app.get("/", async (req, reply) => {
   return reply.status(200).send({ msg: "Deu certo" });
 });
 
-export default async (req: FastifyRequest, res: FastifyReply) => {
-  try {
-    await app.ready();
-    app.server.emit("request", req, res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ error: "Internal Server Error" });
-  }
-};
+export default app;
+
+// export default async (req: FastifyRequest, res: FastifyReply) => {
+//   try {
+//     await app.ready();
+//     app.server.emit("request", req, res);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ error: "Internal Server Error" });
+//   }
+// };
