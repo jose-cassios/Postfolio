fdfe855a7e9fb7ce3de68feded13d82ddf333da7
Antonio Wills <antoniowillissilvasantos@gmail.com>
2025-09-08 17:38:49 -0300

    fix(back-end): finish deploying BETA 0.0.2.

--- src/back-end/api/index.ts
+++ src/back-end/api/index.ts
@@ -1,20 +1,20 @@
-import Fastify, { FastifyReply, FastifyRequest } from "fastify";
-// import { app } from "../src/app";
+// import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+// import { app } from "../src";
 
-const app = Fastify({
-  logger: true,
-});
+// // const app = Fastify({
+// //   logger: true,
+// // });
 
-app.get("/", async (req, reply) => {
-  return reply.status(200).send({ msg: "Deu certo" });
-});
+// // app.get("/", async (req, reply) => {
+// //   return reply.status(200).send({ msg: "Deu certo" });
+// // });
 
-export default async function handler(req: FastifyRequest, res: FastifyReply) {
-  try {
-    await app.ready();
-    app.server.emit("request", req, res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ error: "Internal Server Error" });
-  }
-}
+// export default async function handler(req: FastifyRequest, res: FastifyReply) {
+//   try {
+//     await app.ready();
+//     app.server.emit("request", req, res);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ error: "Internal Server Error" });
+//   }
+// }
