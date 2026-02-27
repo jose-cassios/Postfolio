01acd2f1035413a9131dc7b750da8610a7540134
jose-cassios <cassios.torres.010@gmail.com>
2026-02-27 14:14:24 -0300

    feat(landing): create features-intro layout

--- src/frontend/src/app/features/landing/landing.ts
+++ src/frontend/src/app/features/landing/landing.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from "./components/hero/hero";
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [Hero],
+  imports: [Hero, MatIconModule],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
