3abb90878c80801e2431cbf80f7e35bcba7b02de
jose-cassios <cassios.torres.010@gmail.com>
2026-08-06 19:55:27 -0300

    refactor(routes): adicionando mais rotas para o ssr

--- src/frontend/src/app/app.routes.server.ts
+++ src/frontend/src/app/app.routes.server.ts
@@ -5,12 +5,32 @@ export const serverRoutes: ServerRoute[] = [
     path: '',
     renderMode: RenderMode.Prerender
   },
-    {
+  {
+    path: 'explorar',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'competicoes',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: 'sobre',
     renderMode: RenderMode.Prerender
   },
-    {
+  {
     path: 'perfil/:username',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'auth/login',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'auth/registro',
     renderMode: RenderMode.Server
   },
   {
@@ -18,7 +38,7 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
-  path: '**',
-  renderMode: RenderMode.Server
-}
+    path: '**',
+    renderMode: RenderMode.Server
+  }
 ];
